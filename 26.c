e4b9c416b847c2752f756ad189a0c255dfac77b2
Eloiza <168553661+Eloiza15@users.noreply.github.com>
2024-08-27 08:07:26 -0300

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+// Nível 2: Algoritmos Intermediários (11-30)
+
+//Soma de Matrizes: Soma duas matrizes.
+
+int main() {
+    setlocale(LC_ALL, ""); 
+
+
+    int i, j, mat1[3][3] = {11,12,13,14,15,16,17,18,19}, mat2[3][3] = {1,2,3,4,5,6,7,8,9}, mat3[3][3];
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            mat3[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            printf("%2d ", mat3[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
